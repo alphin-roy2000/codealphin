546d225e743244820d8548819cd0ab1355f8bfca
alphin-roy2000 <56643242+alphin-roy2000@users.noreply.github.com>
2020-03-17 23:24:25 +0530

    f1

--- code.dart
+++ code.dart
@@ -1,6 +1,21 @@
-List<TechLearners> stack1 = <TechLearners>[];List<Mentors> stack2 = <Mentors>[];void main(){  }
+List<TechLearners> stack1 = <TechLearners>[];
+List<Mentors> stack2 = <Mentors>[];
+void main(){  }
 
 
-class TechLearners {  TechLearners({this.interest, });  final interest;  addStacks(){    stack1.add(TechLearners(interest: interest));  }}
+class TechLearners {  
+  TechLearners({this.interest, });
+  final interest;  
+  addStacks(){ 
+    stack1.add(TechLearners(interest: interest));
+  }
+}
 
-class Mentors {  Mentors(this.expertise);  int availabletime;  final expertise;  addStacks(){    stack2.add(Mentors(expertise));  }}
+class Mentors {
+  Mentors(this.expertise); 
+  int availabletime; 
+  final expertise;
+  addStacks(){ 
+    stack2.add(Mentors(expertise));  
+  }
+}
